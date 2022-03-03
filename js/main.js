1e62e7b2ab3adace930c1667f1a6bc62a909128a
diana <radmandian@gmail.com>
2022-03-03 17:04:45 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,554 @@
+let product1 = {
+  name: "Dress",
+  desc: "All-Over Floral Print Tiered Dress with Elbow Sleeves and Tie-Up",
+  price: 5,
+  id: 1,
+  overview:
+    "Showcasing eye-catching floral print  against a bright background,  this tiered dress with elbow sleeves will soon become your",
+  seeMore:
+    "favourite. Made by using quality fabric, this dress  has a good finish and is high on comfort too. The wrap style design and the tie-ups elevate the appeal of this dress.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Elbow Sleeves",
+    item3: "Design : Floral",
+    item4: "Length : Maxi",
+    item5: "Occasion : Casual",
+    item6: "Model Wears : M",
+    item7:
+      "Model Stats : Bust 81 cms, Waist 61 cms, Hips 89 cms, Height 172 cms",
+  },
+  imgSrc1: "img/product1.jpg",
+  imgSrc2: "img/product1-1.jpg",
+  imgSrc3: "img/product1-2.jpg",
+  imgSrc4: "img/product1-3.jpg",
+};
+let product2 = {
+  name: "Pants",
+  desc: "Colour Block Jog Pants with Pockets and Drawstring Closure",
+  price: 300,
+  id: 2,
+
+  overview:
+    "Flaunting a colour block pattern, this pair of jog pants makes for a worthy grab if you're a trendy person.",
+  seeMore:
+    "The athleisure pair comes with a full length and high-rise waist with a drawstring closure. Pockets on the sides add to the utility brought to you by this pair.",
+  desList: {
+    item1: "Style : Joggers",
+    item2: "Fit : Regular",
+    item3: "Design : Solid",
+    item4: "Length : Full Length",
+    item5: "Occasion : Athleisure",
+    item6: "Model Wears : S",
+    item7: "Model Stats : Bust 84 cms, Waist 64 cms, Hips 90 cms,",
+  },
+  imgSrc1: "img/product2.jpg",
+  imgSrc2: "img/product2-1.jpg",
+  imgSrc3: "img/product2-2.jpg",
+  imgSrc4: "img/product2-3.jpg",
+};
+
+let product3 = {
+  name: "Dress",
+  desc: "Textured Midi Tiered Shirt Dress with Short Sleeves and Belt Tie-Ups",
+  price: 520,
+  id: 3,
+  overview:
+    "Add more casual options to your wardrobe with this beautifully crafted tiered shirt dress. Highlighting a textured finish, belt tie-ups, ",
+  seeMore:
+    "neutral tone and v-neckline, this trendy piece is known for the great fit it offers. Furthermore, the button closure makes it easy to wear.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Short Sleeves",
+    item3: "Design : Textured",
+    item4: "Length : Midi",
+    item5: "Material : Synthetic",
+    item6: "Occasion : Casual",
+    item7: "Closure : Button Up",
+  },
+  imgSrc1: "img/product3.jpg",
+  imgSrc2: "img/product3-1.jpg",
+  imgSrc3: "img/product3-2.jpg",
+  imgSrc4: "img/product3-3.jpg",
+};
+
+let product4 = {
+  name: "Denim Jeans",
+  desc: "All-Over Floral Print High-Rise Denim Jeans with Pockets and Zip Closure",
+  price: 400,
+  id: 4,
+  overview:
+    "Rejuvenate your casual wear collection by including these floral print denim jeans in your wardrobe. ",
+  seeMore:
+    " It features belt loops with a zip closure that provide a customised fit, while the pocket and the panelled detail add to its interest. Pair it with a T-shirt and sneakers for a chic look.",
+  desList: {
+    item1: "Style : Denim",
+    item2: "Fit : Mom Fit",
+    item3: "Design : Printed",
+    item4: "Waist Type : High-Rise",
+    item5: "Length : Ankle Length",
+    item6: "Material : Cotton Blend",
+    item7: "Model Stats : Bust 81 cms, Waist 61 cms, Hips 86 cms, Height",
+  },
+  imgSrc1: "img/product4.jpg",
+  imgSrc2: "img/product4-1.jpg",
+  imgSrc3: "img/product4-2.jpg",
+  imgSrc4: "img/product4-3.jpg",
+};
+
+let product5 = {
+  name: "Dress",
+  desc: "All-Over Floral Print Tiered Dress with Elbow Sleeves and Tie-Up",
+  price: 580,
+  id: 5,
+  overview:
+    "Showcasing eye-catching floral print  against a bright background,  this tiered dress with elbow sleeves will soon become your",
+  seeMore:
+    "favourite. Made by using quality fabric, this dress  has a good finish and is high on comfort too. The wrap style design and the tie-ups elevate the appeal of this dress.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Elbow Sleeves",
+    item3: "Design : Floral",
+    item4: "Length : Maxi",
+    item5: "Occasion : Casual",
+    item6: "Model Wears : M",
+    item7:
+      "Model Stats : Bust 81 cms, Waist 61 cms, Hips 89 cms, Height 172 cms",
+  },
+  imgSrc1: "img/product1.jpg",
+  imgSrc2: "img/product1-1.jpg",
+  imgSrc3: "img/product1-2.jpg",
+  imgSrc4: "img/product1-3.jpg",
+};
+
+let product6 = {
+  name: "Dress",
+  desc: "All-Over Floral Print Tiered Dress with Elbow Sleeves and Tie-Up",
+  price: 580,
+  id: 6,
+  overview:
+    "Showcasing eye-catching floral print  against a bright background,  this tiered dress with elbow sleeves will soon become your",
+  seeMore:
+    "favourite. Made by using quality fabric, this dress  has a good finish and is high on comfort too. The wrap style design and the tie-ups elevate the appeal of this dress.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Elbow Sleeves",
+    item3: "Design : Floral",
+    item4: "Length : Maxi",
+    item5: "Occasion : Casual",
+    item6: "Model Wears : M",
+    item7:
+      "Model Stats : Bust 81 cms, Waist 61 cms, Hips 89 cms, Height 172 cms",
+  },
+  imgSrc1: "img/product1.jpg",
+  imgSrc2: "img/product1-1.jpg",
+  imgSrc3: "img/product1-2.jpg",
+  imgSrc4: "img/product1-3.jpg",
+};
+
+let product7 = {
+  name: "Dress",
+  desc: "All-Over Floral Print Tiered Dress with Elbow Sleeves and Tie-Up",
+  price: 580,
+  id: 7,
+  overview:
+    "Showcasing eye-catching floral print  against a bright background,  this tiered dress with elbow sleeves will soon become your",
+  seeMore:
+    "favourite. Made by using quality fabric, this dress  has a good finish and is high on comfort too. The wrap style design and the tie-ups elevate the appeal of this dress.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Elbow Sleeves",
+    item3: "Design : Floral",
+    item4: "Length : Maxi",
+    item5: "Occasion : Casual",
+    item6: "Model Wears : M",
+    item7:
+      "Model Stats : Bust 81 cms, Waist 61 cms, Hips 89 cms, Height 172 cms",
+  },
+  imgSrc1: "img/product1.jpg",
+  imgSrc2: "img/product1-1.jpg",
+  imgSrc3: "img/product1-2.jpg",
+  imgSrc4: "img/product1-3.jpg",
+};
+
+let product8 = {
+  name: "Dress",
+  desc: "All-Over Floral Print Tiered Dress with Elbow Sleeves and Tie-Up",
+  price: 5,
+  id: 8,
+  overview:
+    "Showcasing eye-catching floral print  against a bright background,  this tiered dress with elbow sleeves will soon become your",
+  seeMore:
+    "favourite. Made by using quality fabric, this dress  has a good finish and is high on comfort too. The wrap style design and the tie-ups elevate the appeal of this dress.",
+  desList: {
+    item1: "Style : Tiered",
+    item2: "Sleeve Length : Elbow Sleeves",
+    item3: "Design : Floral",
+    item4: "Length : Maxi",
+    item5: "Occasion : Casual",
+    item6: "Model Wears : M",
+    item7:
+      "Model Stats : Bust 81 cms, Waist 61 cms, Hips 89 cms, Height 172 cms",
+  },
+  imgSrc1: "img/product1.jpg",
+  imgSrc2: "img/product1-1.jpg",
+  imgSrc3: "img/product1-2.jpg",
+  imgSrc4: "img/product1-3.jpg",
+};
+
+// array of product objects
+
+// array of product objects
+
+let products_opjs = [
+  product1,
+  product2,
+  product3,
+  product4,
+  product5,
+  product6,
+  product7,
+  product8,
+];
+
+/**
+ * function called on laod index,html to fill it with products
+ * process ==> read the products_opjs array anf write the in the pagr
+ */
+function fillHtml() {
+  let doc = document.getElementById("doc-container");
+
+  for (let i = 0; i < products_opjs.length; i++) {
+    str = `<div class="product  shadow-lg">
+<div class="image">
+   <img src= ${products_opjs[i].imgSrc1}
+  alt="" onmouseenter="changePic(this)" onmouseout="picBack(this)">
+  </div>
+  <h4 id="p${i + 1}-name" class="prod-name">${products_opjs[i].name}</h4>
+  <p id="p${i + 1}-discrip" class="prod-descrip">${products_opjs[i].desc}</p>
+  <p id="p${i + 1}-price" class="price">${products_opjs[i].price} EGP</p>
+  <button  onclick="openProductPage(${
+    products_opjs[i].id
+  })" class="btn">Show Details</button>
+  
+  
+  <button  onclick=" addToCard(${
+    products_opjs[i].id
+  })" class="btn" >Add to Cart <i class="fas fa-cart-arrow-down pl-2"></i></button>
+ 
+  </div>`;
+
+    doc.innerHTML += str;
+  }
+}
+
+/**
+ * function that takes : *
+ * @param {*} p_num  is the product number
+ * @param {*} btn  is the button  html element
+ *
+ * and show the details of this product
+ *
+ */
+/**
+ * function is called when the product is hoverd(onmouseenter())
+ * @param {*} img
+ * change the img src path to another img
+ */
+function changePic(img) {
+  let str = img.src.slice(0, img.src.length - 4);
+
+  img.src = str + "-1.jpg";
+}
+
+/**
+ * function is called when the mouse leave the product img (onmouseout())
+ * @param {*} img
+ * get back the orignal img
+ */
+
+function picBack(img) {
+  let arr = img.src.split("-1");
+  let newSrc = "";
+  for (const item of arr) {
+    newSrc += item;
+  }
+
+  img.src = newSrc;
+}
+
+/**
+ * function called onclick addtocart button
+ * process ==> get the Id's of clicked item and update the number beside
+ *            the cart in the nav
+ */
+
+let itemsAdded = [];
+
+let num_itemsAdded = 0;
+
+function addToCard(productID) {
+  if (itemsAdded.includes(productID)) {
+    alert("added alredy!");
+  } else {
+    itemsAdded.push(productID);
+    num_itemsAdded++;
+
+    document.getElementById("items-added").innerHTML = num_itemsAdded;
+    localStorage.setItem("items", itemsAdded.join(" "));
+  }
+}
+
+/**
+ * function called onclick on cart icon in the nav bar
+ * process ==> get the id's of clicked items and display them in new page
+ *
+ */
+
+function openCart() {
+  var x = localStorage.getItem("items");
+  let itemsInCart = x.split(" ");
+  let cart = document.getElementById("cart");
+
+  for (const productID of itemsInCart) {
+    let str = `
+
+ <div class=" row item shadow-sm  my-2" id="cartItem${productID}">
+                            <div class="item-img col-3 col-lg-2 d-flex align-items-center ">
+                                <img src=${
+                                  products_opjs[productID - 1].imgSrc1
+                                }>
+                            </div>
+                            <div class="col-lg-10 col-9 ">
+                                <div class="row">
+                                    <div class="details col-md-5 mr-4 p-0 ">
+                                        <a href="product-details.html">
+                                            <p class="title m-3 ">
+                                        ${products_opjs[productID - 1].desc}
+
+                                            </p>
+                                        </a>
+                                      
+                                    </div>
+                                    <div class="col-md-3 d-flex align-items-center ">
+                                        <div class="quantity buttons_added">
+                                          
+                                            <button class="plus-mines" onclick="fun(-1,'${productID}')">-</button>
+                                           
+                                            <input id ="q${productID}" type="number" step="1" min="1" max="" name="quantity" value="0"
+                                            title="Qty" class="input-text qty text" size="4" pattern=""
+                                            inputmode="" />
+                                            <button class="plus-mines" onclick="fun(+1,'${productID}')">+</button>
+                                        </div>
+                                    </div>
+                                    <div class="price py-2 col-md-3 d-flex align-items-center">
+                                    <div class="pr-2"  id="price${productID}">
+                                       ${
+                                         products_opjs[productID - 1].price
+                                       } EGP 
+                                    </div>
+                                    <div  id="dis${productID}">
+                                    
+                                 </div>
+                                 </div>
+                                </div>
+                                <div class="row pb-3">
+                                   
+                                    <div class="col-sm-6 col-12 ">
+                                        <a href="#cart" class="remove btn" onclick="removeItemFromCart('cartItem${productID}')">
+                                            Remove <i class="fas fa-trash-alt"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+`;
+
+    cart.innerHTML += str;
+  }
+}
+// **********************************************************************88
+//     js fuction of (product.html)
+//********************************************************************* */
+
+/**
+ * function to show and hide div
+ * @param {*}  a span element
+ */
+function hide_showText(element) {
+  if (element.innerHTML === "See less") {
+    document.getElementById("see-more").style.display = "none";
+    document.getElementById("des-list").style.display = "none";
+    document.getElementById("dots").innerHTML = "....";
+    element.innerHTML = "See more >>>";
+  } else {
+    document.getElementById("see-more").style.display = "block";
+    document.getElementById("des-list").style.display = "block";
+    document.getElementById("dots").innerHTML = "";
+    element.innerHTML = "See less";
+  }
+}
+
+/**
+ * function that color the chozen size
+ * @param {*} chosen_size
+ */
+function chooseSize(chosen_size) {
+  let newSize = chosen_size.innerHTML;
+
+  let sizeList = document.getElementById("sizes").children;
+  for (const item of sizeList) {
+    if (newSize === item.innerHTML) {
+      item.style.backgroundColor = "#f05a5b";
+    } else {
+      item.style.backgroundColor = "#2B2D42";
+    }
+  }
+}
+
+/**
+ * function called onclick show details button to open the product in a new page
+ *
+ */
+function openProductPage(p_num) {
+  var newWin = window.open("product1.html");
+  newWin.onload = function () {
+    for (let index = 0; index < products_opjs.length; index++) {
+      if (p_num === products_opjs[index].id) {
+        newWin.document.getElementById("p1-name").innerHTML =
+          products_opjs[index].name;
+        newWin.document.getElementById("p1-price").innerHTML = products_opjs[
+          index
+        ].price += " EGP";
+        newWin.document.getElementById("p1-overview").innerHTML =
+          products_opjs[index].overview;
+        newWin.document.getElementById("see-more").innerHTML =
+          products_opjs[index].seeMore;
+
+        var ul = newWin.document.getElementById("des-list");
+        let arr_detailes = Object.values(products_opjs[index].desList);
+        for (const i of arr_detailes) {
+          var li = newWin.document.createElement("li");
+
+          li.appendChild(newWin.document.createTextNode(i));
+          ul.appendChild(li);
+        }
+
+        newWin.document.getElementById("current").src =
+          products_opjs[index].imgSrc1;
+
+        newWin.document.getElementById("img1").src =
+          products_opjs[index].imgSrc2;
+        newWin.document.getElementById("img2").src =
+          products_opjs[index].imgSrc3;
+        newWin.document.getElementById("img3").src =
+          products_opjs[index].imgSrc4;
+      }
+    }
+  };
+}
+
+/**
+ * function is called when click the small images to show in the big image
+ * @param {*} new_pic
+ */
+let currentPic_path = "";
+let old_path = "";
+
+function changeCurrent(new_pic) {
+  currentPic_path = document.getElementById("current").src;
+
+  old_path = currentPic_path;
+
+  document.getElementById("current").src = new_pic.src;
+  new_pic.src = old_path;
+}
+
+/////////////////////////////
+//    the cart page
+//////////////////////////////
+
+function removeItemFromCart(cart_item_num) {
+  var x = localStorage.getItem("items");
+  let itemsInCart = x.split(" ");
+  let itemNumber = cart_item_num.slice(-1);
+  console.log(cart_item_num);
+
+  let index = itemsInCart.indexOf(itemNumber);
+
+  itemsInCart.splice(index, 1);
+  localStorage.setItem("items", itemsInCart.join(" "));
+  let itemInCart = document.getElementById(cart_item_num);
+  itemInCart.remove();
+}
+
+var x = 0;
+let currentID = "";
+let buttonsClicked = [];
+
+function fun(y, id) {
+  let p = 0;
+  buttonsClicked.push(id);
+
+  if (
+    buttonsClicked.length > 1 &&
+    buttonsClicked[buttonsClicked.length - 1] !==
+      buttonsClicked[buttonsClicked.length - 2]
+  ) {
+    x = 0;
+  }
+  let diss = document.getElementById(`dis${id}`);
+
+  let price = products_opjs[id - 1].price;
+
+  let productsPrice = document.getElementById(`price${id}`);
+  console.log(price);
+  p = price;
+  let quantity = document.getElementById(`q${id}`);
+
+  x += y;
+
+  if (x >= 0) {
+    quantity.value = x;
+
+    p = p * parseFloat(quantity.value);
+    productsPrice.innerHTML = p + " EGP";
+
+    if (x > 5) {
+      diss.style.color = "red";
+      diss.style.textDecoration = "line-through";
+      diss.style.fontSize = "12px";
+      diss.textContent = p + " EGP";
+      p = p - (10 * p) / 100;
+      productsPrice.innerHTML = p + " EGP";
+    } else {
+      diss.textContent = "";
+    }
+  }
+}
+
+
+////////////////////////////////////////
+function darkmode() {
+  document.getElementById("nav").classList.toggle("bg-dark");
+  document.getElementById("body").classList.toggle("dark-back-g");
+
+  let names = document.querySelectorAll(".prod-name");
+  let p = document.querySelectorAll(".prod-descrip");
+  let btns = document.querySelectorAll(".btn");
+  let products = document.querySelectorAll(".product");
+  let prices = document.querySelectorAll(".price");
+  for (let i = 0; i < names.length; i++) {
+    names[i].classList.toggle("light-text");
+    p[i].classList.toggle("light-text");
+    products[i].classList.toggle("colored-backg");
+    prices[i].classList.toggle("dark-text");
+  }
+}
+
+function darkmode2() {
+  document.getElementById("nav").classList.toggle("bg-dark");
+  document.getElementById("body").classList.toggle("dark-back-g");
+
+}
